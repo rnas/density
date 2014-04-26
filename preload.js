7b4cda20a470c00a286d5efc1df660cc5bae8439
Raphael Naswaty <rnaswaty@gmail.com>
2014-04-26 20:20:20 -0300

    add imagens sprite

--- preload.js
+++ preload.js
@@ -1,3 +1,7 @@
 function preload() {
-	console.log('Preload');
+
+	game.load.image('bola', 	'/imgs/bolinha.png');
+	game.load.image('fundo', 	'/imgs/background.png');
+
+	console.log('Preload feito');
 };
