25c0327880f585c36d51431b16ca23a2dc2be673
Marco Bruno <marco.bruno.br@gmail.com>
2014-05-01 08:04:21 -0300

    Alert do Game Over trocado por game pausado. E melhorado um pouco a movimentação das pecas

--- javascript/load.js
+++ javascript/load.js
@@ -19,6 +19,7 @@ Game.Load.prototype = {
 		this.add.sprite(0, 0, 'scenario');
 
 		pieces = this.add.group();
+		pieces.enableBody = true;
 		this.createPiece();
 
 		cursors = this.input.keyboard.createCursorKeys();
@@ -26,28 +27,30 @@ Game.Load.prototype = {
 	update: function () {
 		this.physics.arcade.collide(pieces, pieces, function () {
 			if (pieces.children[pieces.children.length-1].body.touching.down && pieces.children[pieces.children.length-1].body.position.y <= 150){
-				alert('Game Over');
+				// alert('Game Over'); // Mover para tela de Game Over ou Pontuação.
+				this.paused = true;
 			} else if (pieces.children[pieces.children.length-1].body.touching.down) {
-
 				this.createPiece();
 			}
 		}, null, this);
 
 		if (piece.body.onFloor()) {
+			piece.body.velocity = 0;
 			this.createPiece();
 		}
 
 		if (cursors.left.isDown) {
-			piece.body.velocity.x = -150; 
+			piece.body.velocity.x = -150;
 		} else if (cursors.right.isDown) {
 			piece.body.velocity.x = 150;
-		}		
+		} else {
+			piece.body.velocity.x = 1;
+		}
 	},
 	createPiece: function () {
 		name = pieces.children.length;
 		piece = pieces.create(120, 0, 'rock');
 		piece.name = 'Rock_' + name;
-		this.physics.arcade.enable(piece);
 		piece.body.velocity.y = 200;
 		piece.body.mass = 0;
 		piece.body.bounce = 0;
